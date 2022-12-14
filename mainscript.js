d67f456612aee9c5e6683bfd07f6bb6441ffa5bf
Kyopil <97451762+Kyopil@users.noreply.github.com>
2022-12-14 19:19:24 -0300

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,5 @@
+// Scroll Down Effects
+window.addEventListener("scroll", function(){
+    var header = document.querySelector(".navbar");
+        header.classList.toggle("sticky", window.scrollY > 0);
+    })
